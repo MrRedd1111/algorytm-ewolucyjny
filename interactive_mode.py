3809d6a9fb60b144ba0a2e9be9115a84786b25a9
MrRedd1111 <67207688+MrRedd1111@users.noreply.github.com>
2026-01-17 12:54:43 +0100

    Update interactive mode description in comments
    Użycie poprawnej polszczyzny w komentarzu

--- interactive_mode.py
+++ interactive_mode.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Mode interaktywny - dostosuj parametry i uruchom algorytm
+Tryb interaktywny - ustal parametry i uruchom algorytm
 """
 
 import numpy as np
